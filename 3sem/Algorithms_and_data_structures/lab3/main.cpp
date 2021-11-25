188d9acf5ec36e76062fea3e0dde302ec5c1a1d7
Andrey <75383288+Andrey529@users.noreply.github.com>
2021-11-25 04:14:57 +0300

    done example of working program

--- 3sem/Algorithms_and_data_structures/lab3/main.cpp
+++ 3sem/Algorithms_and_data_structures/lab3/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include "headers/queue/queue.h"
-#include "headers/stack/stack.h"
 #include "headers/binaryTreeSearch/binaryTreeSearch.h"
+
 int main() {
 
     binaryTreeSearch<int> tree;
@@ -19,53 +18,28 @@ int main() {
     tree.insert(0);
     tree.insert(9);
 
+    std::cout << "Depth-first iterator:" << std::endl;
+    iterator<elemOfBinaryTreeSearch<int>> *treeIteratorDepth = tree.create_dft_iterator();
+    while (treeIteratorDepth->hasNext()) {
+        std::cout << treeIteratorDepth->next().getData() << ' ';
+    }
+    std::cout << std::endl;
 
+
+    std::cout << "Breadth-first iterator:" << std::endl;
     iterator<elemOfBinaryTreeSearch<int>> *treeIteratorBreadth = tree.create_bft_iterator();
     while (treeIteratorBreadth->hasNext()) {
         std::cout << treeIteratorBreadth->next().getData() << ' ';
     }
     std::cout << std::endl;
 
-    iterator<elemOfBinaryTreeSearch<int>> *treeIteratorDepth = tree.create_dft_iterator();
-    while (treeIteratorDepth->hasNext()) {
-        std::cout << treeIteratorDepth->next().getData() << ' ';
-    }
 
+    std::cout << "Find item with value 4 = " << tree.contains(4) << std::endl;
 
-////    std::cout << tree.contains(4) << std::endl;
-//
-//    tree.remove(8);
+    std::cout << "Remove item with value 4" << std::endl;
+    tree.remove(4);
 
-
-//    {
-//        queue<int> queue(0);
-//        for (int i = 1; i <= 5; i++) {
-//            queue.push(i);
-//        }
-//
-//        elemOfQueue<int> *front = queue.getFront();
-//        elemOfQueue<int> *back = queue.getBack();
-//        std::cout << "Queue is empty:" << queue.isEmpty() << std::endl;
-//        std::cout << "Queue size = " << queue.getSize() << std::endl;
-//        queue.pop();
-//        queue.pop();
-//        std::cout << "Queue size = " << queue.getSize() << std::endl;
-//    }
-//    {
-//        stack<int> stack(0);
-//        for (int i = 1; i <= 5; i++) {
-//            stack.push(i);
-//        }
-//
-//        elemOfStack<int> *front = stack.getFront();
-//        elemOfStack<int> *back = stack.getBack();
-//        std::cout << "Stack is empty:" << stack.isEmpty() << std::endl;
-//        std::cout << "Stack size = " << stack.getSize() << std::endl;
-//        stack.pop();
-//        stack.pop();
-//        std::cout << "Stack size = " << stack.getSize() << std::endl;
-//
-//    }
+    std::cout << "Find item with value 4 = " << tree.contains(4) << std::endl;
 
     return 0;
 }
