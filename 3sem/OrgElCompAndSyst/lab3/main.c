b647be03bf98b8179d91ca8b0f499e9a82af9bfe
Andrey <75383288+Andrey529@users.noreply.github.com>
2021-11-30 22:44:01 +0300

    da

--- 3sem/OrgElCompAndSyst/lab3/main.c
+++ 3sem/OrgElCompAndSyst/lab3/main.c
@@ -4,7 +4,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-void drawGraph(double *argument, double *result, unsigned size, double scaleX, double scaleY, unsigned skipX, unsigned skipY)
+void drawFunction(double *X, double *Y, unsigned size, double coefficientX, double coefficientY, unsigned passX, unsigned passY, double start)
 {
     int i;
     unsigned x1, x2, y1, y2;
@@ -12,98 +12,103 @@ void drawGraph(double *argument, double *result, unsigned size, double scaleX, d
     setcolor(RED);
     for (i = 1; i < size; i++)
     {
-        x1 = (unsigned)(argument[i-1]*scaleX);
-        x2 = (unsigned)(argument[i]*scaleX);
-        y1 = getmaxy()-(unsigned)(result[i-1]*scaleY);
-        y2 = getmaxy()-(unsigned)(result[i]*scaleY);
-        line(x1-skipX, y1+skipY, x2-skipX, y2+skipY);
+        x1 = (unsigned)(X[i-1]*coefficientX);
+        x2 = (unsigned)(X[i]*coefficientX);
+        y1 = getmaxy()-(unsigned)(Y[i-1]*coefficientY);
+        y2 = getmaxy()-(unsigned)(Y[i]*coefficientY);
+
+        if (X[i-1] < start) continue;
+
+        line(x1-passX, y1+passY, x2-passX, y2+passY);
     }
     return;
 }
 
-void drawField(double *argument, double *result, const unsigned size, double scaleX, double scaleY, unsigned clipoff)
-{
-    int i, x, y, inc = 8;
-    char text[8];
 
-    setcolor(GREEN);
-    line(clipoff, getmaxy()-clipoff, clipoff, clipoff-15);
-    line(clipoff, getmaxy()-clipoff, getmaxx()-clipoff+15, getmaxy()-clipoff);
+void drawCoordinatePlane(double *X, double *Y, const unsigned size, double coefficientX, double coefficientY, unsigned border){
+    int i, x, y, interval = 10;
+    char text[30];
+    setcolor(LIGHTGRAY);
+    line(border, getmaxy()-border, border, border-15);
+    line(border, getmaxy()-border, getmaxx()-border+15, getmaxy()-border);
 
     setlinestyle(3, 0, 1);
-    for (i = 0; i <= inc; i++)
+    for (i = 0; i <= interval; i++)
     {
-        x = (int)((argument[size-1]-argument[0])/inc*i*scaleX);
-        y = (int)((result[size-1]-result[0])/inc*i*scaleY);
 
-        setcolor(LIGHTGRAY);
-        line(x+clipoff, getmaxy()-clipoff+5, x+clipoff, clipoff-15);
-        line(clipoff-5, getmaxy()-y-clipoff, getmaxx()-clipoff+15, getmaxy()-y-clipoff);
+        x = (int)((X[size-1]-X[0])/interval*i*coefficientX);
+        y = (int)((Y[size-1]-Y[0])/interval*i*coefficientY);
 
-        setcolor(WHITE);
-        sprintf(text, "%.2f", (argument[size-1]-argument[0])/inc*i+argument[0]);
-        outtextxy(x+16, getmaxy()-clipoff+5, text);
-        sprintf(text, "%.2f", (result[size-1]-result[0])/inc*i+result[0]);
-        outtextxy(clipoff+3, getmaxy()-y-clipoff-11, text);
+        line(x+border, getmaxy()-border+5, x+border, border-15);
+        line(border-5, getmaxy()-y-border, getmaxx()-border+15, getmaxy()-y-border);
+
+        sprintf(text, "%.2f", (X[size-1]-X[0])/interval*i+X[0]);
+        outtextxy(x+16, getmaxy()-border+5, text);
+        sprintf(text, "%.2f", (Y[size-1]-Y[0])/interval*i+Y[0]);
+        outtextxy(border+3, getmaxy()-y-border-11, text);
     }
-    return;
 }
 
-void drawMaxValueWindow(double *result, const unsigned size)
+void drawMaxValueOfFunction(double *Y, const unsigned size)
 {
     unsigned i;
     double maximum;
     char text[30];
 
     setviewport(120, 40, 360, 80, 1);
-    maximum = result[0];
+    maximum = Y[0];
     for (i = 1; i < size; i++)
-        if (result[i] > maximum) maximum = result[i];
-    setcolor(WHITE);
+        if (Y[i] > maximum) maximum = Y[i];
+    setcolor(YELLOW);
     setlinestyle(0, 0, 1);
     setfillstyle(SOLID_FILL, DARKGRAY);
     rectangle(0, 0, 239, 39);
-    floodfill(1, 1, WHITE);
+    floodfill(1, 1, YELLOW);
     sprintf(text, "Maximum value is %f", maximum);
     outtextxy(20, 18, text);
 }
 
 int main()
 {
-    const double startPoint = 3*M_PI/2, endPoint = 15*M_PI, step = M_PI/8;
-    const unsigned size = (unsigned)((endPoint-startPoint)/step) + 1;
-    double *argument, *result, maximum;
-    int i, gD, gM;
-    unsigned scaleX, scaleY, skipX, skipY, clipoff = 40;
+    const double start = 3*M_PI/2, end = 15*M_PI, step = M_PI/8;
+    const unsigned size = (unsigned)(end/step) + 1;
+    double *X, *Y;
+    int i, graphDriver, graphMode;
+    unsigned coefficientX, coefficientY, passX, passY, border = 20;
 
-    detectgraph(&gD, &gM);
-    initgraph(&gD, &gM, "C:\\BGI");
-    if (graphresult() != grOk) return 255;
-
-    argument = (double*)malloc(size*sizeof(double));
-    result = (double*)malloc(size*sizeof(double));
-
-    argument[0] = startPoint;
-    result[0] = pow(sin(argument[0]/2),2)+sqrt(argument[0]);
-
-    for (i = 1; i<size; i++)
-    {
-        argument[i] = argument[i-1]+step;
-        result[i] = pow(sin(argument[i]/2),2)+sqrt(argument[i]);
+    detectgraph(&graphDriver, &graphMode);
+    initgraph(&graphDriver, &graphMode, "C:\\BGI");
+    if (graphresult() != grOk) {
+        char error[15];
+        sprintf(error,"Graphic error.");
+        outtextxy(1, 1, error);
+        return -1;
     }
-    scaleX = (getmaxx()-2*clipoff)/(endPoint-startPoint);
-    scaleY = (getmaxy()-2*clipoff)/(result[size-1]-result[0]);
-    skipX = (unsigned)(argument[0]*scaleX) - clipoff;
-    skipY = (unsigned)(result[0]*scaleY) - clipoff;
 
-    drawGraph(argument, result, size, scaleX, scaleY, skipX, skipY);
-    drawField(argument, result, size, scaleX, scaleY, clipoff);
-    drawMaxValueWindow(result, size);
+    X = (double*)malloc(size*sizeof(double));
+    Y = (double*)malloc(size*sizeof(double));
+
+    X[0] = 0;
+    Y[0] = pow(sin(X[0]/2),2)+sqrt(X[0]);
+
+    for (i = 1; i < size; i++)
+    {
+        X[i] = X[i-1]+step;
+        Y[i] = pow(sin(X[i]/2),2)+sqrt(X[i]);
+    }
+    coefficientX = (getmaxx()-2*border)/end;
+    coefficientY = (getmaxy()-2*border)/(Y[size-1]-Y[0]);
+    passX = (unsigned)(X[0]*coefficientX) - border;
+    passY = (unsigned)(Y[0]*coefficientY) - border;
+
+    drawFunction(X, Y, size, coefficientX, coefficientY, passX, passY, start);
+    drawCoordinatePlane(X, Y, size, coefficientX, coefficientY, border);
+    drawMaxValueOfFunction(Y, size);
 
     getch();
 
-    free(argument);
-    free(result);
+    free(X);
+    free(Y);
     closegraph();
     return 0;
-}
+}
